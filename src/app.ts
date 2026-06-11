2a3f68195078c8e0d4de97c89c3c4cd77fe54b7f
Faith Collison <faithcollison@yahoo.co.uk>
2026-06-11 08:59:00 +0100

    add global error handling to app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { DB } from './db/database';
 import { initSchema } from './db/schema';
 import { paymentsRouter } from './routes/payments';
@@ -14,6 +14,11 @@ export function createApp(db: DB) {
   app.use('/payments', paymentsRouter(db));
   app.use('/webhooks', webhooksRouter(db));
   app.use('/ledgers', ledgersRouter(db));
+
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  });
 
   return app;
 }
